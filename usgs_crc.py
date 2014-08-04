2f908b0884e016f96f85e68dd446cc762b3648a4
citizendelta <dghallau@gmail.com>
2014-08-03 22:13:11 -0600

    Create usgs_crc.py
    this script simply builds a URL, opens that URL, then writes the resulting csv file to a file on the local filesystem

---
+++ usgs_crc.py
@@ -0,0 +1,23 @@
+import urllib2
+import urllib
+import time
+import csv
+
+data = {}
+data['format'] = 'csv'
+data['extension'] = 'csv'
+data['analysis'] = 'yes'
+data['search'] = 'Search'
+data['offset'] = '0'
+data['formation'] = 'NIOBRARA'
+url_values = urllib.urlencode(data)
+base = 'http://my.usgs.gov/crcwc/search/cores'
+full_url = base + '?' + url_values
+print full_url
+response = urllib2.urlopen(full_url)
+
+timestr = time.strftime("%Y%m%d")
+csv_name = timestr + '_usgscrc.csv'
+
+date_file = open(csv_name, 'w')
+print>>data_file, response.read()
