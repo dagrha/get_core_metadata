aa0a3f721c4749fa10a13060729823ee7a8aa48b
citizendelta <dghallau@gmail.com>
2014-08-19 09:19:20 -0600

    difflib added
    Added the difflib library to facilitate line-by line comparisons.  Also
    modifed the code substantially but now the script actually works.

--- compare_csv.py
+++ compare_csv.py
@@ -1,32 +1,17 @@
 import csv
 import time
 from datetime import date, timedelta
+import difflib
 
-corenums_yest = []
-topdepth_yest = []
-corenums_today = []
-topdepth_today = []
 timestr = time.strftime("%Y%m%d")
 yesterday = date.today() - timedelta(1)
 csv_yest = yesterday.strftime('%Y%m%d') + '_usgscrc.csv'
 csv_today = timestr + '_usgscrc.csv'
 
-with open(csv_yest, 'rb') as csvfile1:
-    corereader1 = csv.reader(csvfile1)
-    for row in corereader1:
-        corenums_yest.append(row[7])
-        topdepth_yest.append(row[24])
+file1 = open(csv_yest, 'r')
+file2 = open(csv_today, 'r')
 
-with open(csv_today, 'rb') as csvfile2:
-    corereader2 = csv.reader(csvfile2)
-    for row in corereader2:
-        corenums_today.append(row[7])
-        topdepth_today.append(row[24])
-        
-index_yest = corenums_yest + '-' + topdepth_yest
-index_today = corenums_today + '-' + corenums_today
+diff = difflib.ndiff(file1.readlines(), file2.readlines())
 
-s = set(index_today)
-differences = [x for x in index_yest if x not in s]
-
-print differences
+delta = ''.join( x for x in diff if x.startswith('- ') or x.startswith('+ '))
+print delta
